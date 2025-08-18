5f3b1891e8a25abc631787b1980f3f5a3b12af33
JasCoder <85804831+Fanu2@users.noreply.github.com>
2025-08-18 16:27:37 -0500

    Create networks.py

---
+++ networks.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+        self.main = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(64, 3, kernel_size=3, padding=1)
+        )
+
+    def forward(self, x):
+        return self.main(x)
